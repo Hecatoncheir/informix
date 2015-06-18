3640ad416c91ab7446e0173390586fee76a2e51b
Rasarts <rasart.pro@gmail.com>
2015-06-18 11:04:19 +0300

    relative assets :true

--- config.rb
+++ config.rb
@@ -9,14 +9,21 @@ css_dir = "web/stylesheets"
 sass_dir = "web/stylesheets"
 images_dir = "web/images"
 javascripts_dir = "web/scripts"
-fonts_dir = "informix/web/fonts"
+fonts_dir = "web/fonts"
+
+# http_path = "http://rasarts.github.io/informix/"
+# css_dir = "stylesheets"
+# sass_dir = "stylesheets"
+# images_dir = "images"
+# javascripts_dir = "scripts"
+# fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
